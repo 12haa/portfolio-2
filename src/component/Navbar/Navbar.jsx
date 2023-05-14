1cd2b037777bf548c5a1c01fd7e736fba96a194b
12haa <mkhaefi2500@yahoo.com>
2023-05-14 11:29:24 -0700

    Added Hambergur

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import Logo from "../../assets/Logo.png";
 import Close from "../../assets/close.png";
 const Navbar = () => {
+  const [toggle, setToggle] = useState(true);
   return (
     <div className="fixed top-0 left-0 right-0 bg-[#282c33]">
-      <div className="max-w-[1560px] mx-auto flex  justify-between  my-2 items-center">
+      <div className=" px-2 max-w-[1560px] mx-auto flex  justify-between  my-2 items-center">
         <div className="left flex gap-2 items-center font-bold text-white text-base">
           {/* Logo */}
           <div className="image">
@@ -14,9 +15,15 @@ const Navbar = () => {
           Mohammad
         </div>
         <div className="right flex items-center">
-          <div className="menu flex fixed md:static left-0 right-0 top-0 bottom-0">
+          <div
+            className={`menu duration-300 flex-col justify-center md:flex-row flex fixed  w-full ${
+              !toggle
+                ? `right-[-100%] top- bottom-0  `
+                : ` right-0 top-0 bottom-0`
+            }  bg-[#282c33]`}
+          >
             <a href="#">
-              <li className="text-[#abb2bf] list-none  mx-4">
+              <li className="text-[#abb2bf] text-[32px] md:text-base my-4 md:my-auto   list-none  mx-4">
                 {" "}
                 <span className="text-[#c77bdd] font-medium">#</span>
                 Home{" "}
@@ -24,32 +31,35 @@ const Navbar = () => {
             </a>
             <a href="#">
               {" "}
-              <li className="text-[#abb2bf] list-none mx-4">
+              <li className="text-[#abb2bf]  text-[32px] md:text-base  my-4 md:my-auto list-none mx-4">
                 {" "}
                 <span className="text-[#c77bdd] font-medium">#</span>
                 Works{" "}
               </li>
             </a>
             <a href="#">
-              <li className="text-[#abb2bf] mx-4 list-none ">
+              <li className="text-[#abb2bf] text-[32px] md:text-base my-4 md:my-auto   mx-4 list-none ">
                 {" "}
                 <span className="text-[#c77bdd] font-medium">#</span>
                 About-Me{" "}
               </li>
             </a>
             <a href="#">
-              <li className="text-[#abb2bf] list-none mx-4">
+              <li className="text-[#abb2bf]  text-[32px] md:text-base my-4 md:my-auto  list-none mx-4">
                 {" "}
                 <span className="text-[#c77bdd] font-medium">#</span>
                 Contact-Me{" "}
               </li>
             </a>
-            <div className="close block md:hidden">
+            <div
+              onClick={() => setToggle(false)}
+              className="close block  absolute w-5 right-3 top-3"
+            >
               <img src={Close} alt="close Button" />
             </div>
           </div>
           {/* CLose / Open Buttons */}
-          <div>
+          <div onClick={() => setToggle(true)}>
             <div className="open w-8 block md:hidden">
               <div className="w-full h-[1px] my-2 rounded-r-3xl bg-[#d9d9d9]"></div>
               <div className="w-full  h-[1px] my-2 rounded-r-3xl bg-[#d9d9d9]"></div>
