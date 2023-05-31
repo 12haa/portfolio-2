1ae0a6c4e72fbaf4084ddff753081eae024ea744
12haa <mkhaefi2500@yahoo.com>
2023-05-31 12:16:59 -0700

    Downgraded Framer Motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Route, Routes, Router, useLocation } from "react-router";
 import "./App.css";
 import Footer from "./component/Footer/Footer";
@@ -8,21 +8,23 @@ import About_Page from "./Pages/About_page";
 import Projects_Page from "./Pages/Projects_page";
 import Contact_Page from "./Pages/Contact_page";
 import Home_Page from "./Pages/Home_page";
-import { AnimatePresence, useScroll, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
+// import { useScroll } from "framer-motion";
 import { useState } from "react";
 
 const App = () => {
-  const location = useLocation();
-  const { scrollYProgress } = useScroll();
-  const [completed, setCompleted] = useState(0);
-  const [clicked, setCLicked] = useState();
-  const transition = "width 1s ease-in-out";
+  // const location = useLocation();
+  // const { scrollYProgress } = useScroll();
+  // const [completed, setCompleted] = useState(0);
+  // const [clicked, setCLicked] = useState();
+  // const transition = "width 1s ease-in-out";
 
   return (
-    <div className=" bg-[#282c33]    " >
+    <div className=" bg-[#282c33]    ">
       <motion.div
         className="progress-bar"
-        style={{ scaleX: scrollYProgress }}
+        // style={{ scaleX: scrollYProgress }}
       />
       <Navbar />
 
