e0fd24cf21f21317f99f3f143f183190bfa0f5fb
Mohammad Khaefi <mkhaefi2500@yahoo.com>
2023-07-08 23:36:15 +0430

    Added Fourth project

--- src/component/Projects/dummy.jsx
+++ src/component/Projects/dummy.jsx
@@ -1,11 +1,12 @@
 import EditedOne from "./images/Edited-one.jpg";
 import EditedTwo from "./images/Edited-two.jpg";
 import EditedThree from "./images/Edited-three.jpg";
+import EditedFour from "./images/EditedFour.png";
 const Projects = [
   {
     id: 1,
     githubUrl: "",
-    LiveDemoUrl :"https://3-d-portfolio-khaki.vercel.app/",
+    LiveDemoUrl: "https://3-d-portfolio-khaki.vercel.app/",
     img: EditedOne,
     langs: "Html , Css(Tailwind) , Js(React)",
     tittle: "Porfolio Idea",
@@ -28,6 +29,15 @@ const Projects = [
     langs: "Html , Css(Tailwind)  , Js(React)",
     tittle: "Simple Netflix Clone",
     disc: "A Movie-App Project With Firebase",
+  },
+  {
+    id: 4,
+    githubUrl: "https://github.com/12haa/Chatty",
+    LiveDemoUrl: "https://chatty-gold.vercel.app/login",
+    img: EditedFour,
+    langs: "Html , scss  , Js(React) , Firebase",
+    tittle: "Realtime ChatApp",
+    disc: "A Realtime Chat-App For Online Chatting",
   },
 ];
 export default Projects;
